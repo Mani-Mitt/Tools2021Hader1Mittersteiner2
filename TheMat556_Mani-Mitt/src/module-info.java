1e8969869df2296c873f0c79b8302d43e943b1cb
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2021-05-05 11:01:10 +0200

    create a greater conflict

--- TheMat556_Mani-Mitt/src/module-info.java
+++ TheMat556_Mani-Mitt/src/module-info.java
@@ -1,6 +1,8 @@
 public void thisIsANewMethod() { }
 
 module dummy_android {
+java.lang.System.out.print( 6 );
+java.lang.System.out.println();
 java.lang.System.out.print( 4 );
 java.lang.System.out.println();
 java.lang.System.out.println(11);
